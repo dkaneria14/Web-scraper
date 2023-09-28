5b2ef9a45007a88d2703363e97dd674139e10b6e
hanagkhan <hanagkhan11@gmail.com>
2023-09-28 15:08:27 -0400

    frontend react app created

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Typography sx={{ mt: 3 }} align='center' color='primary' variant="h5">Hana hello to react</Typography>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
