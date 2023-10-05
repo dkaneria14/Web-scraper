2dabdeef69345d36ed591c9b49bc4a816dae2013
hanagkhan <hanagkhan11@gmail.com>
2023-10-05 11:28:45 -0400

    install dependencies and functioning page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Typography } from '@mui/material';
 
@@ -6,19 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Typography sx={{ mt: 3 }} align='center' color='primary' variant="h5">Hana hello to react</Typography>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
