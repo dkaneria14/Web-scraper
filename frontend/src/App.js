1ffcb1c0565af9ab92d966080acd7e9d7643864e
hanagkhan <hanagkhan11@gmail.com>
2023-10-06 00:05:46 -0400

    added chips

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,64 @@
 import './App.css';
-import { Typography } from '@mui/material';
+import { Typography, Stack, Chip } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
+import logo from "./logotest.png"
+import { useState } from 'react';
 
 function App() {
+
+  const [tempStockList, setTempStockList] = useState([]);
+  const [searchStock, setSearchStock] = useState("");
+
+  const handleClick = () => {
+    console.info('You clicked the Chip.');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="Logo" width="400" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Buy when it makes sense.
         </p>
         <Paper
           component="form"
           sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
         >
-          
           <InputBase
             sx={{ ml: 1, flex: 1 }}
-            placeholder="Search Google Maps"
-            inputProps={{ 'aria-label': 'search google maps' }}
+            placeholder="Search Stocks"
+            inputProps={searchStock}
           />
           <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
             <SearchIcon />
           </IconButton>
-          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-          
         </Paper>
-        <Typography sx={{ mt: 3 }} align='center' color='primary' variant="h5">Hana hello to react</Typography>
-
+        <Stack direction="row" spacing={1} margin="15px">
+          <Chip
+            // label="Clickable Deletable"
+            label="Apple Inc." color="primary"
+            onClick={handleClick}
+          />
+          <Chip
+            // label="Clickable Deletable"
+            label="NVIDIA Corp" color="primary"
+            onClick={handleClick}
+          />
+          <Chip
+            // label="Clickable Deletable"
+            label="TD Bank" color="primary"
+            onClick={handleClick}
+          />
+          <Chip
+            // label="Clickable Deletable"
+            label="Microsoft Corp" color="primary"
+            onClick={handleClick}
+          />
+        </Stack>
+        {/* <Typography sx={{ mt: 3 }} align='center' color='primary' variant="h5">Hana hello to react</Typography> */}
       </header>
     </div>
   );
