ef801e49a305b85b0ba403949dec4d88271be022
Andre <andredsouza15@gmail.com>
2023-10-12 14:45:30 -0400

    Potential CORS error fix

--- amazonapi.py
+++ amazonapi.py
@@ -1,8 +1,22 @@
 import yfinance as yf
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database.database import DataBase
 
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8000"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/my-first-api")
 
