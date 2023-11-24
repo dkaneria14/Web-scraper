110c447e367346c47ef2ae25e38be8556567ae32
Mohammad Ismail <mohammadn.ismail@mail.utoronto.ca>
2023-11-23 21:17:17 -0500

    Build and Deploy CI/CD

--- backend/stockWatchAPI.py
+++ backend/stockWatchAPI.py
@@ -91,4 +91,4 @@ def update_stock_prices():
         stock_info = yfinance_instance.info
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("stockWatchAPI:app", host="0.0.0.0", port=8000, reload=True)
--- deployImages.sh
+++ deployImages.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 export RASE=stockwatch.cloud
-baseDirectory=$(basename "`pwd`")
 repository=$STOCKWATCH_DOCKERHUB_REPO
 docker compose -f compose.yml build
-docker tag $baseDirectory-server:latest $repository:server
-docker tag $baseDirectory-client:latest $repository:client
+docker tag stockwatch-server:latest $repository:server
+docker tag stockwatch-client:latest $repository:client
 docker push $repository:client
 docker push $repository:server
