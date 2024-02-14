49a3c86c36b416eb3bb3585962efd1bd385a4eda
Mohammad Ismail <mohammadn.ismail@mail.utoronto.ca>
2024-02-14 14:18:14 -0500

    Replace missing stock list with an alternative

--- misc/stockList.py
+++ misc/stockList.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-fortune_500_url = "https://s3.amazonaws.com/oneclickdata/Fortune_500.xlsx"
+fortune_500_url = "https://filebin.net/5awrqt2ax0xfzbda/Fortune_500_Plus.xlsx"
+# Source File (Copied to Drive): https://docs.google.com/spreadsheets/d/16kH6ebwgHymzFuIfhROPAa6GLXClAhOrdkdLcbviNck/edit?usp=sharing
 fortune_500_df = pd.read_excel(fortune_500_url)
 
-stock_symbols = fortune_500_df['Symbol'].tolist()
+# stock_symbols = fortune_500_df['Ticker Symbol'].tolist()
 
-print(stock_symbols)
+json_data = fortune_500_df.set_index("Ticker Symbol")["Company Name"].to_dict()
+
+print(json_data)
